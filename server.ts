c1e4d290eb1d71212fd504a128fb865e294696bd
Owen_Davies <owen.davies@airfordable.com>
2019-06-25 08:03:47 -0500

    Ditch try-catch, simplified error middleware

--- server.ts
+++ server.ts
@@ -14,36 +14,23 @@ serv.get('/', function(req, res, next) {
 
 serv.post('/', function(req, res, next) {
     const data = req.body;
-    try {
-        res.json(todo.addItem(data.description, data.dueAt));
-    } catch (e) {
-        return next(e);
-    }
+    res.json(todo.addItem(data.description, data.dueAt));
 });
 
 serv.put('/:id/done', function(req, res, next) {
     const id: string = req.params.id;
-    try {
-        todo.checkItem(parseInt(id));
-        res.status(200).end();
-    } catch (e) {
-        return next(e);
-    }
+    todo.checkItem(parseInt(id));
+    res.status(200).end();
 });
 
 serv.delete('/:id', function(req, res, next) {
     const id: string = req.params.id;
-    try {
-        res.send(todo.removeItem(parseInt(id)));
-    } catch (e) {
-        return next(e);
-    }
+    res.send(todo.removeItem(parseInt(id)));
 });
 
 serv.use(function(err, req, res, next) {
     console.error(err.message);
-    if (!err.statusCode) { err.statusCode = 500; }
-    res.status(err.statusCode).send(err.message);
+    res.status(err.statusCode || 500).send(err.message);
 });
 
 serv.listen(port, () => console.log(`Todo app listening on port ${port}`));
